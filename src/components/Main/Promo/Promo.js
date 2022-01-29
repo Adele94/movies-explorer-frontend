d6fb9fd7d760a74aa7b14480578c11accadbff91
Adel Shavalieva <adel.shavalieva@yandex.ru>
2022-01-29 22:22:41 +0300

    Add fonts

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -4,7 +4,7 @@ function Promo(props) {
   return (
     <section className="promo">
       <img className="promo__image" src={promoLogo} alt="Картинка шара"/>
-      <h1 className="promo__title">Учебный проект студента факультета <br/> Веб-разработки.</h1>
+      <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
       <h2 className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</h2>
       <button className="promo__more-btn"><a className="promo__more-btn_link" href="#aboutProject">Узнать больше</a></button>
     </section>
