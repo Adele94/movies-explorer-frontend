386698b03982a13056cb56270e4683cc09e9e8b3
Adel Shavalieva <adel.shavalieva@yandex.ru>
2022-02-15 13:41:45 +0300

    Add validation to register and login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,47 +1,69 @@
 import registerLogo from '../../images/header-logo.svg'
-import {Link} from "react-router-dom"
-import React from 'react';
+import { Link } from "react-router-dom"
+import React, { useCallback } from 'react';
 
 function Login(props) {
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [values, setValues] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+  const loginSubmitClassName = (
+    `login__submit ${!isValid ? 'login__submit_disable' : ''}`
+  );
 
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
+  const loginInputClassName = (
+    `login__input ${errors["password"]!=="" ? 'login__input_error' : ''}`
+  );
 
-  function handlePasswordChange(e) {
-    setPassword(e.target.value);
-  }
+  const handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest("form").checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid]
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onLogin({ email, password });
+    let email = values["email"];
+    let password = values["password"];
+    props.onLogin({email, password});
+    resetForm(); 
   }
+
   return (
-    <section  className="login"> 
-      <Link to="/"><img className="login__logo" src={registerLogo} alt="Логотип"/></Link>
+    <section className="login">
+      <Link to="/"><img className="login__logo" src={registerLogo} alt="Логотип" /></Link>
       <h2 className="login__title">Рады видеть!</h2>
-      <form className="login__content" onSubmit={handleSubmit}>
+      <form className="login__content" onSubmit={handleSubmit} >
         <section className="login__input-section">
           <label className="login__label">E-mail</label>
-          <input type="email" className="login__input" onChange={handleEmailChange} required minLength="2" maxLength="200" />
-          <span className="login__input-error login__input-error_active"></span>
+          <input type="email" className="login__input" name="email" onChange={handleChange} required minLength="2" maxLength="200" />
+          <span className="login__input-error login__input-error_active">{errors["email"]}</span>
         </section>
         <section className="login__input-section">
           <label className="login__label">Пароль</label>
-          <input type="password" className="login__input" onChange={handlePasswordChange} required minLength="2" maxLength="200" />
-          <span className="login__input-error login__input-error_active"></span>
+          <input type="password" className={loginInputClassName} name="password" onChange={handleChange} required minLength="5" maxLength="40" />
+          <span className="login__input-error login__input-error_active">{errors["password"]}</span>
         </section>
         <div className="login__button-container" >
-          <button type="submit" className="login__submit"> Войти </button>
+          <button type="submit" className={loginSubmitClassName} disabled={!isValid}> Войти </button>
           <p className="login__question">Ещё не зарегистрированы?
             <Link to="/signup" className="login__link"> Регистрация </Link></p>
         </div >
-      </form> 
+      </form>
     </section>
-);
+  );
 }
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,27 +1,44 @@
 import registerLogo from '../../images/header-logo.svg'
 import {Link} from "react-router-dom"
-import React from 'react';
+import React, { useCallback } from 'react';
 
 function Register(props) {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [name, setName] = React.useState('');
+  const [values, setValues] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+  const registerSubmitClassName = (
+    `register__submit ${!isValid ? 'register__submit_disable' : ''}`
+  );
 
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
+  const registerInputClassName = (
+    `register__input ${errors["password"]!=="" ? 'register__input_error' : ''}`
+  );
+  
+  const handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest("form").checkValidity());
+  };
 
-  function handlePasswordChange(e) {
-    setPassword(e.target.value);
-  }
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid]
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
+    let name = values["name"];
+    let email = values["email"];
+    let password = values["password"];
     props.onRegister({ name, email, password });
+    resetForm();
   }
   
   return (
@@ -31,21 +48,21 @@ function Register(props) {
       <form className="register__content" onSubmit={handleSubmit}>
         <section className="register__input-section">
           <label className="register__label">Имя</label>
-          <input type="text" className="register__input" onChange={handleNameChange} required minLength="2" maxLength="40" />
-          <span className="register__input-error name-input-error"></span>
+          <input type="text" className="register__input" name="name" onChange={handleChange} required minLength="2" maxLength="40" />
+          <span className="register__input-error name-input-error">{errors["name"]}\</span>
         </section>
         <section className="register__input-section">
           <label className="register__label">E-mail</label>
-          <input type="email" className="register__input" onChange={handleEmailChange} required minLength="2" maxLength="200" />
-          <span className="register__input-error description-input-error"></span>
+          <input type="email" className="register__input" name="email" onChange={handleChange} required minLength="2" maxLength="200" />
+          <span className="register__input-error description-input-error">{errors["email"]}</span>
         </section>
         <section className="register__input-section">
           <label className="register__label">Пароль</label>
-          <input type="password" className="register__input register__input-password" onChange={handlePasswordChange} required minLength="5" maxLength="40" />
-          <span className="register__input-error description-input-error register__input-error_active">Что-то пошло не так...</span>
+          <input type="password" className={registerInputClassName} name="password" onChange={handleChange} required minLength="5" maxLength="40" />
+          <span className="register__input-error description-input-error register__input-error_active">{errors["password"]}</span>
         </section>
         <div className="register__button-container" >
-          <button type="submit" className="register__submit"> Зарегистрироваться </button>
+          <button type="submit" className={registerSubmitClassName}> Зарегистрироваться </button>
           <p className="register__question">Уже зарегистрированы?
             <Link to="/signin" className="register__link"> Войти</Link></p>
         </div >
